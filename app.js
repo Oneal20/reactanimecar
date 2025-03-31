e4254f450e55859a1782c5bd09c1b62eb9f66ee4
Oneal20 <164212272+Oneal20@users.noreply.github.com>
2025-03-31 10:36:53 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './src/screens/HomeScreen';
+import CarDetailScreen from './src/screens/CarDetailScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="CarDetail" component={CarDetailScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
